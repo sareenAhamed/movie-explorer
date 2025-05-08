6fcd52dd01a5cd199be4398a111e51fe2025d182
Sareen Ahamed <sareenahamed40@gmail.com>
2025-05-08 16:45:59 +0530

    Project Initialized

---
+++ src/services/TMDb.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+
+const API_KEY = '2ee3bf845e87c6ddeb65e7a88f523aa7';
+const BASE_URL = 'https://api.themoviedb.org/3/discover/movie?';
+
